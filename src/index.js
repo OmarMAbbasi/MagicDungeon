ac6c27f49cb4e214fe7e1cb34317b4b09ccd774d
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-19 20:46:48 -0700

    CSS animations working

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import Floor from "./classes/floor";
 export function component() {
 	const element = document.createElement("div");
 
-	// Lodash, currently included via a script, is required for this line to work
 	let fb = floorBuilder;
-	// debugger;
-
+	
 	let test = fb(6);
 	let floor = new Floor(6);
 	// element.innerHTML = test;
