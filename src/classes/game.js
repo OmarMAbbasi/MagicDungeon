f7bcdfeaa6e5302dddbf15e6230aaa114e1e73c0
Omar Abbasi <omarmabbasi@gmail.com>
2019-07-21 19:23:03 -0700

    Left/Right smooth

--- src/classes/game.js
+++ src/classes/game.js
@@ -17,15 +17,25 @@ export default class Game {
 			right: false,
 			left: false
 		};
+		this.lastKey = "right";
 		this.grid[1][1].className = "wiz";
+		this.grid[1][1].classList.add("idle-right");
+
+		this.grid[10][10].classList.add("wiz");
+		this.grid[10][10].classList.add("left");
+		this.grid[10][10].classList.add("idle-left");
+
 		// this.walkRight = this.walkRight.bind(this);
 
 		this.row = 0;
 		this.col = 0;
 		this.prev;
 		// this.throtRight = _.throttle(this.walkRight, 750);
-		this.resetHor = _.debounce(this.debHor, 750);
-		this.resetVert = _.debounce(this.debVert, 750);
+		this.resetHor = this.debHor.bind(this);
+		this.resetVert = this.debHor.bind(this);
+
+		// this.resetHor = _.debounce(this.debHor, 750);
+		// this.resetVert = _.debounce(this.debHor, 750);
 		this.throttledMoveRight = _.throttle(this.throttledMoveRight, 750);
 		this.throttledMoveLeft = _.throttle(this.throttledMoveLeft, 750);
 		this.throttledMoveDown = _.throttle(this.throttledMoveDown, 750);
@@ -41,6 +51,7 @@ export default class Game {
 		if (this.col !== 1) {
 			this.col = 1;
 		}
+		this.bounce();
 	}
 
 	throttledMoveLeft() {
@@ -49,6 +60,7 @@ export default class Game {
 		if (this.col !== -1) {
 			this.col = -1;
 		}
+		this.bounce();
 	}
 
 	throttledMoveDown() {
@@ -57,6 +69,7 @@ export default class Game {
 		if (this.row !== 1) {
 			this.row = 1;
 		}
+		this.bounce();
 	}
 
 	throttledMoveUp() {
@@ -65,11 +78,20 @@ export default class Game {
 		if (this.row !== -1) {
 			this.row = -1;
 		}
+		this.bounce();
 	}
 
 	resetAnimation() {
 		let [square, row, col] = Object.values(this.charPos);
-		this.charPos.square.className = "wiz";
+		if (this.lastKey === "left") {
+			square.classList.remove(...square.classList);
+			square.classList.add("wiz");
+			square.classList.add("idle-left");
+		} else if (this.lastKey === "right") {
+			square.classList.remove(...square.classList);
+			square.classList.add("wiz");
+			square.classList.add("idle-right");
+		}
 	}
 	// debtest() {
 	// 	let [square, row, col] = Object.values(this.charPos);
@@ -91,6 +113,13 @@ export default class Game {
 	// }
 
 	debHor(key) {
+		if (key === "left") {
+			this.lastKey = "left";
+			this.lastKey = "left";
+		}
+		if (key === "right") {
+			this.lastKey = "right";
+		}
 		this.col = 0;
 	}
 
@@ -99,6 +128,8 @@ export default class Game {
 	}
 
 	debtest() {
+		debugger;
+		this.idle();
 		let [square, row, col] = Object.values(this.charPos);
 		let pRow = row;
 		let pCol = col;
@@ -106,69 +137,75 @@ export default class Game {
 		col += this.col;
 		let next = this.grid[row][col];
 		let prev = this.charPos.square;
+		prev.classList.remove(...prev.classList);
 
-		next.classList.add("wiz");
 		if (pCol < col) {
+			next.classList.remove(...next.classList);
+			next.className = "wiz";
 			next.classList.add("right");
 		} else if (pCol > col) {
+			next.classList.remove(...next.classList);
+			next.className = "wiz";
 			next.classList.add("left");
+		} else {
+			next.classList.remove(...next.classList);
+			next.className = "wiz";
+
+			next.classList.add("right");
 		}
-		prev.className = "square";
-		this.idle();
 		Object.assign(this.charPos, { row: row, col: col, square: next });
 	}
 
 	bindKeys() {
 		document.addEventListener("keydown", e => {
-			debugger;
-
 			// console.log(e.keyCode);
 			switch (e.keyCode) {
 				case 87: // W
 				case 38:
 					this.throttledMoveUp();
-					this.bounce();
 					break;
 				case 83: // S
 				case 40:
 					this.throttledMoveDown();
-					this.bounce();
 					break;
 				case 65: // A
 				case 37:
 					this.throttledMoveLeft();
-					this.bounce();
 					break;
 				case 68: // D
 				case 39:
 					this.throttledMoveRight();
-					this.bounce();
 					break;
 				default:
 					break;
 			}
 		});
 		document.addEventListener("keyup", e => {
-			debugger;
 			// console.log(e.keyCode);
 			switch (e.keyCode) {
 				case 87: // W
 				case 38:
+					this.bounce.cancel();
 					this.resetVert();
 
 					break;
 				case 83: // S
 				case 40:
+					this.bounce.cancel();
 					this.resetVert();
 
 					break;
 				case 65: // A
 				case 37:
-					this.resetHor();
+					this.bounce.cancel();
+					this.resetHor("left");
+
 					break;
 				case 68: // D
 				case 39:
-					this.resetHor();
+					this.bounce.cancel();
+					this.resetHor("right");
+
 					break;
 				default:
 					break;
